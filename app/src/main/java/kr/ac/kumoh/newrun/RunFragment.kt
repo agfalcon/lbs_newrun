cfab7c2979a9aaed5ad4de048030613bf4d36544
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-04-04 17:46:04 +0900

    기초 프래그먼트 세팅

---
+++ app/src/main/java/kr/ac/kumoh/newrun/RunFragment.kt
@@ -0,0 +1,25 @@
+package kr.ac.kumoh.newrun
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RunFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_run, container, false)
+    }
+
+}
