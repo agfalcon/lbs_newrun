64204a4c2a36443d4e1049f3c781c45baedc45a7
KimGeunBeom <agfalcon12@gmail.com>
2023-04-17 16:26:57 +0900

    [Run]달리기 기능 종료 및 종료 시 서비스 및 데이터 초기화 완료

--- app/src/main/java/kr/ac/kumoh/newrun/TimeRecordData.kt
+++ app/src/main/java/kr/ac/kumoh/newrun/TimeRecordData.kt
@@ -1,8 +1,12 @@
 package kr.ac.kumoh.newrun
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
 @Parcelize
 data class TimeRecordData(
     val time: Int,
     val latitude: Double,
     val longitude: Double
-)
+) : Parcelable
