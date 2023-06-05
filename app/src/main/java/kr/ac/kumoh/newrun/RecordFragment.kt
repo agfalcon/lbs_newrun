75b41edb17caeb96432500c859b7b05a14a8680e
신근재 <60930743+Boknami@users.noreply.github.com>
2023-06-05 23:48:03 +0900

    fragment_record 생성[0605]
    신근재 -> 기록 프래그먼트 페이지 작성 및 이미지 파일들 업로드

--- app/src/main/java/kr/ac/kumoh/newrun/RecordFragment.kt
+++ app/src/main/java/kr/ac/kumoh/newrun/RecordFragment.kt
@@ -1,21 +1,48 @@
 package kr.ac.kumoh.newrun
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.ProgressBar
 
 class RecordFragment : Fragment() {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var listView: ListView
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_record, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_record, container, false)
+        progressBar = view.findViewById(R.id.pgBar)
+        listView = view.findViewById(R.id.listView)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupProgressBar()
+        setupListView()
+    }
+
+    private fun setupProgressBar() {
+        // Set the progress drawable color to yellow
+        progressBar.progressDrawable.setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN)
+
+        // Set the progress value (0-100)
+        progressBar.progress = 30
+    }
+
+    private fun setupListView() {
+        val items = mutableListOf<String>()
+        for (i in 1..10) {
+            items.add("Item $i")
+        }
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+    }
 }
