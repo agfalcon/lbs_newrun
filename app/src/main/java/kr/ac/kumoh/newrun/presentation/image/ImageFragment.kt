99cad488c457f1a26343aff04b2296efa1c5372b
신근재 <60930743+Boknami@users.noreply.github.com>
2023-06-11 00:41:22 +0900

    aos : Image fragment 수행중 (오류존재)

--- app/src/main/java/kr/ac/kumoh/newrun/presentation/image/ImageFragment.kt
+++ app/src/main/java/kr/ac/kumoh/newrun/presentation/image/ImageFragment.kt
@@ -1,15 +1,22 @@
 package kr.ac.kumoh.newrun.presentation.image
 
+<<<<<<< Updated upstream
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
 import android.util.Base64
 import android.util.Log
+=======
+import android.content.Intent
+import android.os.Bundle
+import android.util.DisplayMetrics
+>>>>>>> Stashed changes
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+<<<<<<< Updated upstream
 import android.widget.Button
 import android.widget.ImageView
 import kotlinx.coroutines.CoroutineScope
@@ -24,14 +31,77 @@ import kr.ac.kumoh.newrun.data.repository.StableDiffusionService
 class ImageFragment : Fragment() {
     private lateinit var imageButton : Button
     private lateinit var imageView: ImageView
+=======
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.airbnb.lottie.animation.content.Content
+import com.kakao.sdk.common.util.SdkLogLevel
+import kr.ac.kumoh.newrun.R
+import kr.ac.kumoh.newrun.domain.data.VisitPathData
+import kr.ac.kumoh.newrun.presentation.HomeActivity
+import org.w3c.dom.Text
+
+
+class ImageFragment : Fragment() {
+    private lateinit var PathListView: ListView
+
+    var PathList = arrayListOf<VisitPathData>(
+        VisitPathData("2023-06-01", "03시간 3분 기록","sample"),
+        VisitPathData("2023-06-02", "13시간 3분 기록","sample"),
+        VisitPathData("2023-06-03", "23시간 3분 기록","sample"),
+        VisitPathData("2023-06-01", "03시간 3분 기록","sample"),
+        VisitPathData("2023-06-02", "13시간 3분 기록","sample"),
+        VisitPathData("2023-06-03", "23시간 3분 기록","sample"),
+    )
+>>>>>>> Stashed changes
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_image, container, false)
+        PathListView = view.findViewById(R.id.PathListView)
 
+<<<<<<< Updated upstream
 
         return inflater.inflate(R.layout.fragment_image, container, false)
+=======
+        val PathAdapter = PathAdapter(requireContext(), PathList)
+        PathListView.adapter = PathAdapter
+
+        PathListView.setOnItemClickListener{parent, view, position, id ->
+            Toast.makeText(requireContext(), "${position+1} 번째 클릭했슴다", Toast.LENGTH_SHORT).show()
+            showDialog()
+        }
+
+        view.findViewById<TextView>(R.id.createTitleTextView).setOnClickListener{
+            showDialog()
+        }
+        return view
+    }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        val inflater: LayoutInflater = layoutInflater
+
+        // 다이얼로그 뷰 초기화
+        val dialogView = inflater.inflate(R.layout.dialog_suc_signup, null)
+
+        builder.setView(dialogView)
+        val alertDialog: AlertDialog = builder.create()
+
+        alertDialog.setOnShowListener {
+            val displayMetrics = DisplayMetrics()
+            //windowManager.defaultDisplay.getMetrics(displayMetrics)
+            val width = displayMetrics.widthPixels
+            val height = displayMetrics.heightPixels
+            val dialogWidth = (width * 0.85).toInt() // 디바이스 가로 크기의 60%
+            val dialogHeight = (height * 0.55).toInt() // 디바이스 세로 크기의 80%
+            alertDialog.window?.setLayout(dialogWidth, dialogHeight)
+        }
+
+        alertDialog.show()
+>>>>>>> Stashed changes
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
