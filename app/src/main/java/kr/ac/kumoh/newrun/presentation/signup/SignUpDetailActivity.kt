05d1df1529dd66bc005980722760f843b80b54be
신근재 <60930743+Boknami@users.noreply.github.com>
2023-06-11 17:27:45 +0900

    AOS : 로그인,회원가입 길이 제한 추가 및 변경

--- app/src/main/java/kr/ac/kumoh/newrun/presentation/signup/SignUpDetailActivity.kt
+++ app/src/main/java/kr/ac/kumoh/newrun/presentation/signup/SignUpDetailActivity.kt
@@ -11,11 +11,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kr.ac.kumoh.newrun.data.model.LoginData
 import kr.ac.kumoh.newrun.data.model.SignUp
+import kr.ac.kumoh.newrun.data.model.SocialSignUp
+import kr.ac.kumoh.newrun.data.model.snsLoginData
 import kr.ac.kumoh.newrun.data.repository.LoginService
 import kr.ac.kumoh.newrun.data.repository.SignUpService
 import kr.ac.kumoh.newrun.databinding.ActivitySignUpDetailBinding
 import kr.ac.kumoh.newrun.domain.data.UserInfo
 import kr.ac.kumoh.newrun.presentation.HomeActivity
+import kr.ac.kumoh.newrun.presentation.login.LoginActivity
 import kr.ac.kumoh.newrun.presentation.login.LoginNRActivity
 
 class SignUpDetailActivity : AppCompatActivity() {
@@ -33,47 +36,79 @@ class SignUpDetailActivity : AppCompatActivity() {
         val userEmail = intent.getStringExtra("userEmail").toString()
 
         binding.btnSignUpComplete.setOnClickListener {
-            val targetKm = binding.etKm.text.toString()
-            val name = binding.etName.text.toString()
-            val nickname = binding.etNickname.text.toString()
+            if(binding.etName.text.toString().length < 1) {
+                Toast.makeText(applicationContext, "이름을 작성 후 제출해주세요!", Toast.LENGTH_SHORT).show()
+            } else if(binding.etNickname.text.toString().length < 1) {
+                Toast.makeText(applicationContext, "닉네임을 작성 후 제출해주세요!", Toast.LENGTH_SHORT).show()
+            } else if(binding.etKm.text.toString().length < 1) {
+                Toast.makeText(applicationContext, "목표 KM를 작성 후 제출해주세요!", Toast.LENGTH_SHORT).show()
+            } else {
+                val targetKm = binding.etKm.text.toString()
+                val name = binding.etName.text.toString()
+                val nickname = binding.etNickname.text.toString()
 
-            Log.i("정상 회원가입 Id"," ${userId} ")
-            Log.i("정상 회원가입 Pw"," ${userPw} ")
-            Log.i("정상 회원가입 이름"," ${name} ")
-            Log.i("정상 회원가입 별명"," ${nickname} ")
-            Log.i("정상 회원가입 km"," ${targetKm} ")
-            Log.i("정상 회원가입", "email: ${userEmail}")
+                Log.i("정상 회원가입 Id"," ${userId} ")
+                Log.i("정상 회원가입 Pw"," ${userPw} ")
+                Log.i("정상 회원가입 이름"," ${name} ")
+                Log.i("정상 회원가입 별명"," ${nickname} ")
+                Log.i("정상 회원가입 km"," ${targetKm} ")
+                Log.i("정상 회원가입", "email: ${userEmail}")
 
-            Log.i("테스트","로그인 진입")
-            if(userEmail != "null"){
-                //소셜 서버 통신 -> 용구가 나중에 만들면
-                Log.i("테스트","소셜 로그인 수행")
-
-            }
-            else {
-                //자체 로그인
-                Log.i("테스트","자체 로그인 수행")
-                CoroutineScope(
-                    Dispatchers.IO
-                ).launch {
-                    val result = SignUpService().signUp(
-                        SignUp(
-                            email = userId,
-                            userName = name,
-                            nickName = nickname,
-                            password = userPw,
-                            goal_Distance = targetKm.toFloat(),
+                Log.i("테스트","로그인 진입")
+                if(userEmail != "null"){
+                    //소셜 서버 통신 -> 용구가 나중에 만들면
+                    Log.i("테스트","소셜 로그인 수행")
+                    CoroutineScope(
+                        Dispatchers.IO
+                    ).launch {
+                        val result = SignUpService().snsSignUp(
+                            SocialSignUp(
+                                email = userEmail,
+                                userMame = name,
+                                nickName = nickname,
+                                goal_Distance = targetKm.toFloat()
+                            )
                         )
-                    )
-                    Log.i("테스트",result)
-                    if (result == "가입성공") {
-                        val intent = Intent(applicationContext, LoginNRActivity::class.java)
-                        startActivity(intent)
-                    } else {
-                        CoroutineScope(
-                            Dispatchers.Main
-                        ).launch {
-                            Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                        Log.i("테스트",result)
+
+                        if (result == "가입성공") {
+                            val intent = Intent(applicationContext, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            CoroutineScope(
+                                Dispatchers.Main
+                            ).launch {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                }
+                else {
+                    //자체 로그인
+                    Log.i("테스트","자체 로그인 수행")
+                    CoroutineScope(
+                        Dispatchers.IO
+                    ).launch {
+                        val result = SignUpService().signUp(
+                            SignUp(
+                                email = userId,
+                                userName = name,
+                                nickName = nickname,
+                                password = userPw,
+                                goal_Distance = targetKm.toFloat(),
+                            )
+                        )
+                        Log.i("테스트",result)
+                        if (result == "가입성공") {
+                            val intent = Intent(applicationContext, LoginNRActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            CoroutineScope(
+                                Dispatchers.Main
+                            ).launch {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
                 }
